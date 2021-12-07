e7fab72b1739c759b1ed680b533d0e280b307499
LizaZaharenko363 <liza.zaharenko25@gmail.com>
2021-12-07 16:42:46 +0200

    create

---
+++ script.js
@@ -0,0 +1,14 @@
+$(function() {
+	$("#magic").click(function(){
+		document.location.href = "https://lizazaharenko363.github.io/magic8/";
+	});
+	$("#num").click(function(){
+		document.location.href = "https://lizazaharenko363.github.io/Lab/";
+	});
+	$("#card").click(function(){
+		document.location.href = "https://lizazaharenko363.github.io/Cards/";
+	});
+	$("#slot").click(function(){
+		document.location.href = "https://lizazaharenko363.github.io/SlotGame/";
+	});
+});
